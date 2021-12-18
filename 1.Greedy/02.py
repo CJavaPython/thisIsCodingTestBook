85f16dd30285a324a61888e25ccff58c7c91d480
linux_com <woongsu0614@gmail.com>
2021-12-19 02:38:17 +0900

    solve 02.py

---
+++ 1.Greedy/02.py
@@ -0,0 +1,19 @@
+N, M, K = map(int, input().split(" "))
+L = list(map(int, input().split(" ")))
+def bigNum(N, M, K, L):
+  result = []
+  max_index = L.index(max(L))
+  max_val = L.pop(max_index)
+  if M > K:
+    for i in range(K):
+      result.append(max_val)
+    M -= K + 1
+    next_max_val = L[L.index(max(L))]
+    result.append(next_max_val)
+    L.insert(max_index, max_val)
+    result.extend(bigNum(N, M, K, L))
+  else:
+    for i in range(M):
+      result.append(max_val)
+  return result
+print(bigNum(N, M, K, L))
