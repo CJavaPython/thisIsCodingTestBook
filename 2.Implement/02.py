e836acd70ab6c69ca926ca9d063ba68cdca908c2
woongsu0614 <woongsu0614@gmail.com>
2022-02-03 23:36:54 +0900

    implement 02.py, 02-sol.py

--- 2.Implement/02.py
+++ 2.Implement/02.py
@@ -1,1 +1,13 @@
 #정수 N 입력 -> N시 59분 59초까지 숫자 3이 하나라도 포함된 모든 경우의 수 구하기
+x = int(input())
+
+def func(num):
+    result = 0
+    for i in range(num+1):
+        for j in range(60):
+            for k in range(60):
+                if '3' in str(i) + str(j) + str(k):
+                    result+=1
+    return result
+
+print(func(x))
